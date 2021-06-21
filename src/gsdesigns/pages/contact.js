5a6effcc7f5a4bfd654366633acd25e2a755c8e4
kapoor17 <himanshu.kapur17@gmail.com>
2021-06-21 13:56:22 +0530

    edited class to className in contact page

--- src/gsdesigns/pages/contact.js
+++ src/gsdesigns/pages/contact.js
@@ -11,7 +11,7 @@ const ContactUs=()=>{
             <section className="section p-5">
                 <div className="container-md">
                     <div className="heading fs-1 mb-5">Contact Us</div>
-                    <div class="d-flex justify-content-between align-items-start">
+                    <div className="d-flex justify-content-between align-items-start">
                         <div className="contact fs-5 d-flex flex-column justify-content-between align-items-start">
                             <div className="contact-details w-100 d-flex justify-content-start align-items-start">
                                 <FontAwesomeIcon icon={faEnvelope}/>
@@ -28,7 +28,7 @@ const ContactUs=()=>{
                         </div>
                         <div className="form-container py-3 px-4 d-flex flex-column justify-content-center align-items-start">
                                 <p className="instruction">Fill out the form below and our team will be in touch with you shortly.</p>
-                            <form class="form d-flex flex-column justify-content-center align-items-start">
+                            <form className="form d-flex flex-column justify-content-center align-items-start">
                                 <input type="text" id="name" name="name" value="Name" />
                                 <input type="text" id="email" name="email" value="Email" />
                                 <input type="text" id="phone" name="email" value="Phone Number" />
@@ -37,7 +37,7 @@ const ContactUs=()=>{
                                     <input type="text" id="how" name="how" value="How did you find us" />
                                 </div>
                                 <input type="text" id="msg" name="msg" value="Message" />
-                                <input type="submit" value="Submit" class="submit"/>
+                                <input type="submit" value="Submit" className="submit"/>
                             </form>
                         </div>
                     </div>
