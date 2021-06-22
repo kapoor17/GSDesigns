06b0051740a79b377208669e2f49e54d57c45068
kapoor17 <himanshu.kapur17@gmail.com>
2021-06-22 18:31:05 +0530

    added photos for recent-news images

--- src/gsdesigns/components/RecentNewsCard.js
+++ src/gsdesigns/components/RecentNewsCard.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import photo from '../components/carousel-photos/photo3.jpg'
-
 
 const RecentNewsCard=(props)=>{
     return(
-        <div className="col-6 border border-success">
+        <div className="recent-news">
             <img src={props.url} alt="" />
             <h3>TITLE FOR RECENT NEWS</h3>
             <p>date/month</p>
--- src/gsdesigns/pages/homenew.js
+++ src/gsdesigns/pages/homenew.js
@@ -2,6 +2,16 @@ import React,{useEffect,useState} from "react";
 import photo1 from "../components/carousel-photos/photo1.jpg";
 import photo2 from "../components/carousel-photos/photo2.jpg";
 import photo3 from "../components/carousel-photos/photo3.jpg";
+import photoa from "../components/recent-news photos/photoa.jpg";
+import photob from "../components/recent-news photos/photob.jpg";
+import photoc from "../components/recent-news photos/photoc.jpg";
+import photod from "../components/recent-news photos/photod.jpg";
+import photoe from "../components/recent-news photos/photoe.jpg";
+import photof from "../components/recent-news photos/photof.jpg";
+import photog from "../components/recent-news photos/photog.jpg";
+import photoh from "../components/recent-news photos/photoh.jpg";
+import photoi from "../components/recent-news photos/photoi.jpg";
+import photoj from "../components/recent-news photos/photoj.jpg";
 import PageButton from "../components/pageButton";
 import {Carousel, LeftButton, RightButton} from "../components/Carousel"
 import RecentNewsCard from '../components/RecentNewsCard.js'
@@ -9,10 +19,22 @@ import "../css/home.css"
 
 const Home=()=>{
     
-    const [recentNews, getRecentNews]=useState([
-    {url:photo1, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sapiente? Eligendi optio aut eveniet nisi, aspernatur, veniam esse sit voluptatem ullam aperiam aliquid corrupti molestiae?"},
-    {url:photo2, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, delectus. Lorem ipsum dolor sit amet."},
-    {url:photo3,text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nostrum laudantium necessitatibus. Id, dolore mollitia corrupti itaque nulla error incidunt architecto accusantium quod sequi laudantium! Veniam nobis cumque ipsum mollitia, architecto facilis repellendus nulla voluptate"}]);
+    const [recentNewsa, getRecentNewsa]=useState([
+        {url:photoa, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sapiente? Eligendi optio aut eveniet nisi, aspernatur, veniam esse sit voluptatem ullam aperiam aliquid corrupti molestiae?"},
+        {url:photob, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, delectus. Lorem ipsum dolor sit amet."},
+        {url:photoc,text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nostrum laudantium necessitatibus. Id, dolore mollitia corrupti itaque nulla error incidunt architecto accusantium quod sequi laudantium! Veniam nobis cumque ipsum mollitia, architecto facilis repellendus nulla voluptate"},
+        {url:photod, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, delectus. Lorem ipsum dolor sit amet."},
+        {url:photoe, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sapiente? Eligendi optio aut eveniet nisi, aspernatur, veniam esse sit voluptatem ullam aperiam aliquid corrupti molestiae?"},
+        {url:photof, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sapiente? Eligendi optio aut eveniet nisi, aspernatur, veniam esse sit voluptatem ullam aperiam aliquid corrupti molestiae?"}
+    ]);
+
+    const [recentNewsb, getRecentNewsb]=useState([
+        {url:photog, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sapiente? Eligendi optio aut eveniet nisi, aspernatur, veniam esse sit voluptatem ullam aperiam aliquid corrupti molestiae?"},
+        {url:photoh, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, delectus. Lorem ipsum dolor sit amet."},
+        {url:photoi,text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nostrum laudantium necessitatibus. Id, dolore mollitia corrupti itaque nulla error incidunt architecto accusantium quod sequi laudantium! Veniam nobis cumque ipsum mollitia, architecto facilis repellendus nulla voluptate"},
+        {url:photoj, text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, delectus. Lorem ipsum dolor sit amet."}
+    ]);
+    
 
     const images =[photo1,photo2,photo3];
 
@@ -80,7 +102,7 @@ const Home=()=>{
     
     return(
         <>
-            <section className="section carousel p-4 border border-danger">
+            <section className="section carousel p-4">
                 <div className="inner-container">
 
                     <div className="carousel__button-container left is-hidden">
@@ -103,12 +125,24 @@ const Home=()=>{
             </section>
             
 
-            <section className="section p-4 rn-container border border-danger">
+            <section className="section p-4 rn-container">
                 <div className="container-md">
                     <div className="row">
-                        {recentNews.map(news=>(
-                            <RecentNewsCard url={news.url} text={news.text}/>
-                        ))}
+                        <div className="column">
+                            {recentNewsa.map(news=>(
+                                <RecentNewsCard url={news.url} text={news.text}/>
+                            ))}
+                        </div>
+                        <div className="column">
+                            {recentNewsb.map(news=>(
+                                <RecentNewsCard url={news.url} text={news.text}/>
+                            ))}
+                        </div>
+                        <div className="column">
+                            {recentNewsa.map(news=>(
+                                <RecentNewsCard url={news.url} text={news.text}/>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </section>
