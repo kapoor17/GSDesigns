525f40af230278cc9dcbaf2e5456867c234efeed
kapoor17 <himanshu.kapur17@gmail.com>
2021-06-23 17:29:56 +0530

    added form to the bottom and tweaked the recent news cards

--- src/gsdesigns/components/RecentNewsCard.js
+++ src/gsdesigns/components/RecentNewsCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import'../css/recent-news.css'
 
 const RecentNewsCard=(props)=>{
     return(
-        <div className="recent-news">
+        <div className="recent-news mx-2">
             <img src={props.url} alt="" />
-            <h3>TITLE FOR RECENT NEWS</h3>
-            <p>date/month</p>
+            <h3 className="py-2">TITLE FOR RECENT NEWS</h3>
+            <p className="p-0 m-0">date/month</p>
             <p>{props.text}</p>
         </div>
     );
--- src/gsdesigns/pages/homenew.js
+++ src/gsdesigns/pages/homenew.js
@@ -1,4 +1,6 @@
 import React,{useEffect,useState} from "react";
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
 import photo1 from "../components/carousel-photos/photo1.jpg";
 import photo2 from "../components/carousel-photos/photo2.jpg";
 import photo3 from "../components/carousel-photos/photo3.jpg";
@@ -13,12 +15,13 @@ import photog from "../components/recent-news photos/photog.jpg";
 import photoh from "../components/recent-news photos/photoh.jpg";
 import photoi from "../components/recent-news photos/photoi.jpg";
 import photoj from "../components/recent-news photos/photoj.jpg";
+import '../css/contact.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPhone,faEnvelope, faMapPin} from '@fortawesome/free-solid-svg-icons'
 import PageButton from "../components/pageButton";
 import MediaLinks from "../components/MediaLinks";
 import RecentNewsCard from '../components/RecentNewsCard.js'
 import "../css/home.css"
-import Swiper from 'swiper/bundle';
-import 'swiper/swiper-bundle.css';
 
 const Home=()=>{
     
@@ -65,7 +68,7 @@ const Home=()=>{
             </section>
             
             <section className="section p-4">
-                <div className="container-md rn-container">
+                <div className="container-lg rn-container">
                     <div className="row">
                         <div className="column">
                             {recentNewsa.map(news=>(
@@ -81,6 +84,42 @@ const Home=()=>{
                             {recentNewsa.map(news=>(
                                 <RecentNewsCard url={news.url} text={news.text}/>
                             ))}
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="section p-5">
+                <div className="container-md">
+                    <div className="heading fs-1 mb-5">Contact Us</div>
+                    <div className="d-flex justify-content-between align-items-start">
+                        <div className="contact fs-5 d-flex flex-column justify-content-between align-items-start">
+                            <div className="contact-details w-100 d-flex justify-content-start align-items-start">
+                                <FontAwesomeIcon icon={faEnvelope}/>
+                                <p>info@gsdesigns.com.in</p>
+                            </div>
+                            <div className="contact-details w-100 d-flex justify-content-start align-items-start">
+                                <FontAwesomeIcon icon={faPhone}/>
+                                <p>1300 806 809</p>
+                            </div>
+                            <div className="contact-details w-100 d-flex justify-content-start align-items-start">
+                                <FontAwesomeIcon icon={faMapPin}/>
+                                <p>lorem ipsum dolor</p>
+                            </div>
+                        </div>
+                        <div className="form-container py-3 px-4 d-flex flex-column justify-content-center align-items-start">
+                                <p className="instruction">Fill out the form below and our team will be in touch with you shortly.</p>
+                            <form className="form d-flex flex-column justify-content-center align-items-start">
+                                <input type="text" id="name" name="name" value="Name" />
+                                <input type="text" id="email" name="email" value="Email" />
+                                <input type="text" id="phone" name="email" value="Phone Number" />
+                                <div className="flex-overide d-flex">
+                                    <input type="text" id="project-type" name="project-type" value="Project Type" />
+                                    <input type="text" id="how" name="how" value="How did you find us" />
+                                </div>
+                                <input type="text" id="msg" name="msg" value="Message" />
+                                <input type="submit" value="Submit" className="submit"/>
+                            </form>
                         </div>
                     </div>
                 </div>
